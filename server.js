e7c4c916277f64225a4390659e030b1728c4c0ca
hoanganhdev <hoanganhdepzai1566@gmail.com>
2025-10-30 19:25:28 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const PROXY_LIST = [
+    {
+        host: '142.111.48.253',
+        port: 7030,
+        auth: {
+            username: 'fkvtqdtd',
+            password: 'baqho7ocj8fs'
+        }
+    },
+    // Thêm nhiều proxy để tăng tốc độ và độ ổn định
+];
